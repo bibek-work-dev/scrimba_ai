0b092f55c8e0407933e62a54f2afbe31618787a7
Bibek Koirala <bibek.koirala@ebpearls.com>
2025-11-28 15:44:18 +0545

    done @@

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-import openai, { OpenAI } from "openai"
+// import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+dotenv.config();
 
-const openai = new OpenAI({ apiKey: "AIzaSyA8v3M6TxvXepoUtk-JHwNcsReO1h1AONc" })
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain about evolution",
+  });
+
+  console.log("response", response);
+  console.log("response.hora", response.text);
+  console.log("ACTUAL TEXT:", response.text === undefined);
+}
+
+main();
+
+// const genAI = new GoogleGenerativeAI("AIzaSyA8v3M6TxvXepoUtk-JHwNcsReO1h1AONc");
